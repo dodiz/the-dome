785e1f530cabc3581bed71c4115596350f76712d
Dodiz <dodiz69@gmail.com>
2021-09-07 12:53:04 +0200

    added circuit lines

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -12,7 +12,8 @@ class Background extends React.Component {
 	state = {
 		horizontalLines: 0,
 		verticalLinesPositions: [],
-		horizontalLinesPositions: []
+		horizontalLinesPositions: [],
+		circuitLines: []
 	}
 
 	horizontalLineSpace = 15
@@ -33,10 +34,13 @@ class Background extends React.Component {
 			this.verticalLineSpace
 		)
 
+		const circuitLines = this.getCircuitLines()
+
 		this.setState({
 			horizontalLines,
 			verticalLinesPositions,
-			horizontalLinesPositions
+			horizontalLinesPositions,
+			circuitLines
 		})
 	}
 	getLinesPositions(width, space) {
@@ -50,12 +54,104 @@ class Background extends React.Component {
 		}
 		return itemsPositions
 	}
+	getCircuitLines() {
+		const width = window.innerWidth
+		const height = window.innerHeight
+
+		const widthOriginal = 1000
+		const heightOriginal = 600
+
+		const widthScale = width / widthOriginal
+		const heightScale = height / heightOriginal
+
+		// Lines go from left to right when they start in the left half.
+		// And go from right to left when they start in the right half.
+
+		let linesOriginal = [
+			[
+				[31, 80],
+				[45, 98],
+				[478, 98]
+			],
+			[
+				[-10, 136],
+				[567, 136],
+				[597, 96],
+				[867, 96]
+			],
+			[
+				[923, 21],
+				[507, 21],
+				[496, 33],
+				[98, 33],
+				[65, -10]
+			],
+			[
+				[38, 267],
+				[157, 267]
+			],
+			[
+				[1010, 225],
+				[566, 225],
+				[503, 307],
+				[295, 307]
+			],
+			[
+				[88, 340],
+				[362, 340],
+				[372, 354],
+				[854, 354]
+			],
+			[
+				[1010, 368],
+				[908, 368]
+			],
+			[
+				[219, 491],
+				[236, 512],
+				[484, 512]
+			],
+			[
+				[981, 447],
+				[725, 448],
+				[688, 495]
+			],
+			[
+				[855, 536],
+				[618, 535],
+				[579, 485],
+				[-10, 485]
+			],
+			[
+				[71, 448],
+				[104, 405],
+				[292, 405]
+			],
+			[
+				[34, 610],
+				[63, 560],
+				[147, 560],
+				[173, 520]
+			],
+			[
+				[1010, 176],
+				[658, 176]
+			]
+		]
+
+		const lines = linesOriginal.map(line => {
+			return line.map(([x, y]) => [x * widthScale, y * heightScale])
+		})
+
+		return lines
+	}
 
 	render() {
 		const {
 			horizontalLines,
 			verticalLinesPositions,
-			horizontalLinesPositions
+			horizontalLinesPositions,
+			circuitLines
 		} = this.state
 
 		const height = window.innerHeight
@@ -63,7 +159,7 @@ class Background extends React.Component {
 
 		return (
 			<div className="background">
-				<div className="background__linear-container">
+				<div className="background__container background__linear-container">
 					{Array(horizontalLines)
 						.fill()
 						.map((_, index) => (
@@ -75,7 +171,7 @@ class Background extends React.Component {
 						))}
 				</div>
 				<svg
-					className="background__dotted-container"
+					className="background__container background__dotted-container"
 					xmlns="http://www.w3.org/2000/svg">
 					{verticalLinesPositions.map((value, index) => (
 						<line
@@ -98,8 +194,43 @@ class Background extends React.Component {
 						/>
 					))}
 				</svg>
-
-				<div className="background__radial" />
+				<svg
+					className="background__container background__circuit-container"
+					xmlns="http://www.w3.org/2000/svg">
+					{circuitLines.map((line, index) => (
+						<g className="classes.circuit" key={index} data-index={index}>
+							<path
+								className="background__circuit-line"
+								d={line
+									.map(
+										([x, y], pIndex) => `${pIndex === 0 ? "M" : "L"}${x},${y}`
+									)
+									.join(" ")}
+							/>
+							<path
+								className={"background__circuit-line-light"}
+								d={line
+									.map(
+										([x, y], pIndex) => `${pIndex === 0 ? "M" : "L"}${x},${y}`
+									)
+									.join(" ")}
+							/>
+							<circle
+								className={"background__circuit-dot"}
+								cx={`${line[0][0]}px`}
+								cy={`${line[0][1]}px`}
+								r="3px"
+							/>
+							<circle
+								className={"background__circuit-dot"}
+								cx={`${line[line.length - 1][0]}px`}
+								cy={`${line[line.length - 1][1]}px`}
+								r="3px"
+							/>
+						</g>
+					))}
+				</svg>
+				<div className="background__container background__radial" />
 			</div>
 		)
 	}
