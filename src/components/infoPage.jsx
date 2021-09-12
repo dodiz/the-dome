937040f28a998adceba28e0bff366bb6630e1d27
Dodiz <dodiz69@gmail.com>
2021-09-13 00:39:07 +0200

    brand as h3

--- src/components/infoPage.jsx
+++ src/components/infoPage.jsx
@@ -8,24 +8,26 @@ const InfoPage = props => {
 			<Navbar {...props} />
 			<div className="box-container">
 				<FrameBox
+					className="arwes-framebox"
 					animator={{ duration: { enter: 1000 } }}
-					hideShapes
-					className="box__info">
-					<Text as="h2">The Dome Project</Text>
-					<Text as="p">
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
-						ad, nulla doloribus illum nam aliquam temporibus molestias nesciunt
-						laboriosam omnis! Modi ullam similique consequuntur quibusdam?
-						Error, eum nulla dignissimos eos commodi quibusdam provident
-						reprehenderit quis quia quaerat fugit minus doloribus repudiandae,
-						ad architecto excepturi animi cum earum minima! Vero nisi suscipit
-						temporibus consequuntur nihil incidunt atque consequatur, iure
-						asperiores at, quod delectus id magni distinctio sunt nulla placeat
-						voluptatibus ab alias nobis consectetur, quas velit dolorum! Autem
-						cumque velit sapiente unde facere rem et voluptates recusandae quam,
-						ab provident sint, sunt possimus earum, incidunt dolor. Vel ratione
-						qui impedit aliquam!
-					</Text>
+					hideShapes>
+					<div className="box__info">
+						<Text as="h2">The Dome Project</Text>
+						<Text as="p">
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Explicabo ad, nulla doloribus illum nam aliquam temporibus
+							molestias nesciunt laboriosam omnis! Modi ullam similique
+							consequuntur quibusdam? Error, eum nulla dignissimos eos commodi
+							quibusdam provident reprehenderit quis quia quaerat fugit minus
+							doloribus repudiandae, ad architecto excepturi animi cum earum
+							minima! Vero nisi suscipit temporibus consequuntur nihil incidunt
+							atque consequatur, iure asperiores at, quod delectus id magni
+							distinctio sunt nulla placeat voluptatibus ab alias nobis
+							consectetur, quas velit dolorum! Autem cumque velit sapiente unde
+							facere rem et voluptates recusandae quam, ab provident sint, sunt
+							possimus earum, incidunt dolor. Vel ratione qui impedit aliquam!
+						</Text>
+					</div>
 				</FrameBox>
 			</div>
 		</div>
