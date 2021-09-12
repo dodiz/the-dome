48b3c47abb0c55e41fdee569325e9b9b0bc134ab
Dodiz <dodiz69@gmail.com>
2021-09-12 03:14:04 +0200

    added link classes

--- src/components/common/link.jsx
+++ src/components/common/link.jsx
@@ -5,12 +5,14 @@ import { Text } from "@arwes/core"
 import withSound from "../../hoc/withSound"
 
 function LinkComponent(props) {
+	const className = props.className || ""
+
 	return (
 		<Link
 			onMouseOver={() => props.sounds.hover.play()}
 			onClick={() => props.sounds.click.play()}
 			to={props.to}
-			className="link">
+			className={`link ${className}`}>
 			<Text>{props.children}</Text>
 		</Link>
 	)
--- src/tools/arwes/themeProvider.jsx
+++ src/tools/arwes/themeProvider.jsx
@@ -2,20 +2,7 @@ import React from "react"
 import { createTheme } from "@arwes/design"
 import { ArwesThemeProvider } from "@arwes/core"
 
-const theme = createTheme({
-	palette: {
-		// Default theme palette basic colors.
-		tonalOffset: 0.1,
-		primary: { main: "var(--color-secondary)" },
-		secondary: { main: "var(--color-accent)" },
-		success: { main: "var(--color-success" },
-		error: { main: "var(--color-error)" },
-
-		// Default theme palette elevation colors.
-		elevationOffset: 0.025,
-		neutral: { main: "#000" }
-	}
-})
+const theme = createTheme({})
 
 const Themeprovider = ({ children }) => {
 	return <ArwesThemeProvider theme={theme}>{children}</ArwesThemeProvider>
