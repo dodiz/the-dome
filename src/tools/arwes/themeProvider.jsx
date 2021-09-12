141f2a62f307408bddbee29637eee80735c8f7ee
Dodiz <dodiz69@gmail.com>
2021-09-13 00:39:28 +0200

    some tests

--- src/tools/arwes/themeProvider.jsx
+++ src/tools/arwes/themeProvider.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import { createTheme } from "@arwes/design"
 import { ArwesThemeProvider } from "@arwes/core"
 
-const theme = createTheme({})
-
 const Themeprovider = ({ children }) => {
-	return <ArwesThemeProvider theme={theme}>{children}</ArwesThemeProvider>
+	return <ArwesThemeProvider>{children}</ArwesThemeProvider>
 }
 
 export default Themeprovider
