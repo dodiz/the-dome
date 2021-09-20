4e794a2912ea96a596014c0c15adfa3fc36f35e7
Dodiz <dodiz69@gmail.com>
2021-09-20 14:52:37 +0200

    styled explored component

--- src/components/splash/explore.jsx
+++ src/components/splash/explore.jsx
@@ -1,30 +1,17 @@
-import React, { useState } from "react"
-import { Text } from "@arwes/core"
+import React from "react"
 
 import Icon from "../common/icon"
 
 const Explore = () => {
-	const [show, setShow] = useState(false)
-
 	return (
-		<div className="splash__explore" onMouseOver={() => setShow(true)}>
+		<div className="splash__explore">
 			<Icon className="splash__explore-icon" round>
 				<a href="#explore">
-					<svg
-						className="icon"
-						xmlns="http://www.w3.org/2000/svg"
-						fill-rule="evenodd"
-						clip-rule="evenodd"
-						viewBox="0 0 10 10">
+					<svg className="icon" viewBox="0 0 10 10">
 						<path d="M 0 0 L 5 7 L 10 0" />
 					</svg>
 				</a>
 			</Icon>
-			{show && (
-				<Text as="h1" className="splash__explore-heading">
-					Esplora
-				</Text>
-			)}
 		</div>
 	)
 }
--- src/components/splashPage.jsx
+++ src/components/splashPage.jsx
@@ -1,25 +1,45 @@
-import React from "react"
+import React, { Component } from "react"
 
 import Brand from "./brand"
 import FeatureComponent from "./splash/featureComponent"
 import Menu from "./splash/menu"
 import Pegi from "./splash/pegi"
 import Explore from "./splash/explore"
-import { Text } from "@arwes/core"
 
-const SplashPage = () => {
-	return (
-		<div>
-			<main className="splash">
-				<Brand />
-				<Menu />
-				<Pegi />
-				<Explore />
-			</main>
-			<a name="explore" />
-			<FeatureComponent />
-		</div>
-	)
+class SplashPage extends Component {
+	state = {
+		show: false
+	}
+
+	checkScroll() {
+		if (window.scrollY > 200) {
+			window.removeEventListener("scroll", this.checkScroll)
+		}
+		console.log(window.scrollY)
+	}
+
+	componentDidMount() {
+		window.addEventListener("scroll", this.checkScroll)
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this.checkScroll)
+	}
+
+	render() {
+		return (
+			<div>
+				<main className="splash">
+					<Brand />
+					<Menu />
+					<Pegi />
+					<Explore />
+				</main>
+				<a name="explore" />
+				<FeatureComponent />
+			</div>
+		)
+	}
 }
 
 export default SplashPage
