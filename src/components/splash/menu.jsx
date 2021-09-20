d078901cd4a39b6b41cb85ff02c6a633b8c44c23
Dodiz <dodiz69@gmail.com>
2021-09-20 15:58:08 +0200

    fixed menu bug

--- src/components/splash/menu.jsx
+++ src/components/splash/menu.jsx
@@ -21,7 +21,7 @@ export default function SplashMenu() {
 	return (
 		<ul className="list splash-menu">
 			{links.map(link => (
-				<li className="splash-menu__item">
+				<li key={link.label} className="splash-menu__item">
 					<Link className="splash-menu__link" to={link.to}>
 						{link.label}
 					</Link>
