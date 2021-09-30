b063270cb79590c06aaa93bb29318c477b6e31fe
Dodiz <dodiz69@gmail.com>
2021-09-30 15:32:55 +0200

    fixed allignment

--- src/components/infoPage.jsx
+++ src/components/infoPage.jsx
@@ -38,7 +38,7 @@ class InfoPage extends Component {
 					className=""
 					style={{
 						display: "flex",
-						margin: "2rem auto",
+						margin: ".5rem auto",
 						justifyContent: "space-evenly",
 						width: "100%"
 					}}>
@@ -47,31 +47,28 @@ class InfoPage extends Component {
 						onItemSelect={this.handleInfoSelect}
 						selectedItem={this.state.selectedInfo}
 					/>
-
-					<div className="info-box">
-						<FrameBox palette="secondary">
-							<div className="arwes__box">
-								<Text as="h2" className="h2">
-									{this.state.selectedInfo.name}
-								</Text>
-								<Text as="p">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Tempore, sunt quam nostrum est modi ipsam blanditiis deserunt
-									doloribus illum fuga cumque eveniet soluta asperiores vero
-									commodi laboriosam, corporis, omnis minima eum aperiam placeat
-									debitis laborum. Veritatis maxime non incidunt reprehenderit
-									unde fugiat facilis nisi vitae quas laboriosam culpa quasi
-									enim officiis pariatur possimus deserunt, numquam repellendus
-									nulla rem laborum quidem veniam reiciendis temporibus.
-									Praesentium repellat fugiat saepe facere asperiores natus
-									laborum repudiandae dolor dolorem dolorum, quam sit tempora
-									minus laudantium obcaecati, commodi ab voluptates aliquam
-									quasi est aut? Similique consequuntur sed ea odio eius tempore
-									error neque quisquam, recusandae fugit!
-								</Text>
-							</div>
-						</FrameBox>
-					</div>
+					<FrameBox palette="secondary">
+						<div className="arwes__box">
+							<Text as="h2" className="h2">
+								{this.state.selectedInfo.name}
+							</Text>
+							<Text as="p">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Tempore, sunt quam nostrum est modi ipsam blanditiis deserunt
+								doloribus illum fuga cumque eveniet soluta asperiores vero
+								commodi laboriosam, corporis, omnis minima eum aperiam placeat
+								debitis laborum. Veritatis maxime non incidunt reprehenderit
+								unde fugiat facilis nisi vitae quas laboriosam culpa quasi enim
+								officiis pariatur possimus deserunt, numquam repellendus nulla
+								rem laborum quidem veniam reiciendis temporibus. Praesentium
+								repellat fugiat saepe facere asperiores natus laborum
+								repudiandae dolor dolorem dolorum, quam sit tempora minus
+								laudantium obcaecati, commodi ab voluptates aliquam quasi est
+								aut? Similique consequuntur sed ea odio eius tempore error neque
+								quisquam, recusandae fugit!
+							</Text>
+						</div>
+					</FrameBox>
 				</div>
 			</div>
 		)
