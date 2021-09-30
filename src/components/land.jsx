677ccf0afa4c3367b5149209870778c7c33430fa
Dodiz <dodiz69@gmail.com>
2021-09-30 18:46:01 +0200

    added menu animation

--- src/components/land.jsx
+++ src/components/land.jsx
@@ -6,9 +6,30 @@ import {
 	FramePentagon,
 	Figure
 } from "@arwes/core"
+import anime from "animejs"
 import Icon from "./common/icon"
 
 class Land extends Component {
+	componentDidMount() {
+		const delay = 2000
+		const duration = 400
+		const space = 50
+		anime({
+			targets: ".menu__icon",
+			easing: "easeOutCubic",
+			opacity: {
+				delay,
+				duration,
+				value: [0, 1]
+			},
+			top: {
+				value: (_, i, t) => `${space * i}%`,
+				delay: delay + duration * 2,
+				duration
+			}
+		})
+	}
+
 	render() {
 		return (
 			<div
@@ -17,13 +38,14 @@ class Land extends Component {
 					boxSizing: "border-box",
 					padding: ".6rem",
 					height: "100vh",
-					gridTemplate: "1fr / 200px 1fr 80px",
+					gridTemplate: "1fr / 300px 1fr 70px",
 					gridGap: "2rem"
 				}}>
 				<FrameHexagon>UI</FrameHexagon>
 				<FrameLines>mappa</FrameLines>
-				<FrameCorners palette="secondary" style={{ textAlign: "center" }}>
+				<FrameCorners className="menu" palette="secondary">
 					<Icon
+						className="menu__icon"
 						secondary
 						src="/images/pegi/violent.png"
 						style={{ width: "40px" }}
@@ -31,6 +53,7 @@ class Land extends Component {
 					<br />
 					<br />
 					<Icon
+						className="menu__icon"
 						secondary
 						src="/images/pegi/drugs.png"
 						style={{ width: "40px" }}
@@ -38,6 +61,7 @@ class Land extends Component {
 					<br />
 					<br />
 					<Icon
+						className="menu__icon"
 						src="/images/pegi/discrimination.png"
 						secondary
 						style={{ width: "40px" }}
@@ -45,6 +69,7 @@ class Land extends Component {
 					<br />
 					<br />
 					<Icon
+						className="menu__icon"
 						secondary
 						src="/images/pegi/sex.png"
 						style={{ width: "40px" }}
