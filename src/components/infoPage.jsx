76063b5e2ec9f049c1794ac548a334579b12337c
Dodiz <dodiz69@gmail.com>
2021-09-30 15:00:50 +0200

    test info commit

--- src/components/infoPage.jsx
+++ src/components/infoPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import ListGroup from "./common/listGroup"
 import Navbar from "./splash/navbar"
+import { FrameBox, Text } from "@arwes/core"
 
 const items = [
 	{
@@ -22,7 +23,7 @@ const items = [
 
 class InfoPage extends Component {
 	state = {
-		selectedInfo: null
+		selectedInfo: items[0]
 	}
 
 	handleInfoSelect = selectedInfo => {
@@ -33,13 +34,44 @@ class InfoPage extends Component {
 		return (
 			<div>
 				<Navbar stick show={true} />
-				<div className="" style={{ display: "flex" }}>
+				<div
+					className=""
+					style={{
+						display: "flex",
+						margin: "2rem auto",
+						justifyContent: "space-evenly",
+						width: "100%"
+					}}>
 					<ListGroup
 						items={items}
 						onItemSelect={this.handleInfoSelect}
 						selectedItem={this.state.selectedInfo}
 					/>
-					<div className="">test</div>
+
+					<div className="info-box">
+						<FrameBox palette="secondary">
+							<div className="arwes__box">
+								<Text as="h2" className="h2">
+									{this.state.selectedInfo.name}
+								</Text>
+								<Text as="p">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Tempore, sunt quam nostrum est modi ipsam blanditiis deserunt
+									doloribus illum fuga cumque eveniet soluta asperiores vero
+									commodi laboriosam, corporis, omnis minima eum aperiam placeat
+									debitis laborum. Veritatis maxime non incidunt reprehenderit
+									unde fugiat facilis nisi vitae quas laboriosam culpa quasi
+									enim officiis pariatur possimus deserunt, numquam repellendus
+									nulla rem laborum quidem veniam reiciendis temporibus.
+									Praesentium repellat fugiat saepe facere asperiores natus
+									laborum repudiandae dolor dolorem dolorum, quam sit tempora
+									minus laudantium obcaecati, commodi ab voluptates aliquam
+									quasi est aut? Similique consequuntur sed ea odio eius tempore
+									error neque quisquam, recusandae fugit!
+								</Text>
+							</div>
+						</FrameBox>
+					</div>
 				</div>
 			</div>
 		)
