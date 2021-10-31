5df253984c882cbcdc045f758e309eecf4c30766
Dodiz <dodiz69@gmail.com>
2021-10-31 15:58:53 +0100

    added date component

--- src/components/ui.jsx
+++ src/components/ui.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import { FrameCorners, FrameHexagon, FrameLines } from "@arwes/core"
+
 import Brand from "./brand"
 import Icon from "./common/icon"
-import HeaderUI from "./ui-header"
-import "../css/ui.css"
 
-import { FrameCorners } from "@arwes/core"
+import DateUI from "./ui-date"
+import WeatherUI from "./ui-weather"
+import BarsUI from "./ui-bars"
+
+import dome from "../media/dome.png"
+import "../css/ui.css"
 
 class UI extends React.Component {
 	state = {
@@ -39,13 +44,36 @@ class UI extends React.Component {
 					</FrameCorners>
 				</div>
 				<div className="ui__content collapsible__content">
-					<HeaderUI />
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid,
-					tempore similique? Vel assumenda deleniti soluta dolore error adipisci
-					optio. Labore perspiciatis maiores, reiciendis laudantium numquam
-					dolorem aut amet debitis eaque mollitia facilis recusandae ratione
-					tempore, iure officia laborum veniam dolores explicabo. Quibusdam,
-					praesentium quo eos consequatur obcaecati quae illo dolores.
+					<FrameLines
+						largeLineWidth={2}
+						smallLineWidth={4}
+						smallLineLength={20}
+						hideShapes
+						className="ui__header ui__box">
+						<div className="ui__flex">
+							<img
+								className="ui__logo animate__animated animate__bounceIn animate__delay-1s"
+								src={dome}
+							/>
+							<div className="ui__flex-content">
+								<WeatherUI />
+								<div className="ui__line" />
+								<DateUI />
+							</div>
+						</div>
+					</FrameLines>
+					<FrameHexagon className="ui__box">
+						<div className="ui__flex">
+							<img
+								className="ui__pg"
+								src="https://pbs.twimg.com/profile_images/1236608518347788295/IaKy3w-m_400x400.png"
+							/>
+							<div className="ui__box">
+								<BarsUI />
+								<BarsUI secondary />
+							</div>
+						</div>
+					</FrameHexagon>
 				</div>
 			</div>
 		)
