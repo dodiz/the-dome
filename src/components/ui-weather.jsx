c72c1d04c812c72ca1b1d12f0a49ae596d9130d0
Dodiz <dodiz69@gmail.com>
2021-11-03 15:25:36 +0100

    refactor weather

--- src/components/ui-weather.jsx
+++ src/components/ui-weather.jsx
@@ -6,9 +6,9 @@ import SunIcon from "../media/icons/weather/sun.svg"
 class WeatherUI extends React.Component {
 	render() {
 		return (
-			<div className="ui__weather">
+			<div className="weather">
 				<Text as="div">27°C</Text>
-				<img alt="" src={SunIcon} className="ui__weather-icon" />
+				<img alt="" src={SunIcon} className="weather__icon" />
 			</div>
 		)
 	}
