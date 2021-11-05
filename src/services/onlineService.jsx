acb5160cf431f58e2a150be1733358e3385be0d7
Dodiz <dodiz69@gmail.com>
2021-11-05 17:04:58 +0100

    refactor model

--- src/services/onlineService.jsx
+++ src/services/onlineService.jsx
@@ -2,34 +2,54 @@ function getUsers() {
 	const users = [
 		{
 			_id: "10239430",
-			name: "Michael Douglas",
-			img: "https://picsum.photos/200",
+			name: "pinco pallo",
+			pg: {
+				_id: "78473892",
+				name: "Michael Douglas",
+				img: "https://picsum.photos/200"
+			},
 			isAdmin: true
 		},
 		{
 			_id: "102394301",
-			name: "Julia Roberts",
-			img: "https://picsum.photos/200",
+			name: "dodiz",
+			pg: {
+				_id: "847392",
+				name: "Julia Roberts",
+				img: "https://picsum.photos/200"
+			},
 			isAdmin: false,
 			isBusy: true
 		},
 		{
 			_id: "102394302",
-			name: "Jack Nicholson",
-			img: "https://picsum.photos/200",
+			name: "Marzia",
+			pg: {
+				_id: "390348029",
+				name: "Jack Nicholson",
+				img: "https://picsum.photos/200"
+			},
 			isAdmin: true
 		},
 		{
 			_id: "102394303",
-			name: "Will Smith",
-			img: "https://picsum.photos/200",
+			name: "Giuseppe",
+			pg: {
+				_id: "..iuqweioe",
+				name: "Will Smith",
+				img: "https://picsum.photos/200"
+			},
 			isAdmin: false,
 			isBusy: true
 		},
 		{
 			_id: "102394304",
-			name: "Jennifer Lawrence",
-			img: "https://picsum.photos/200",
+			name: "asdrubale",
+			pg: {
+				_id: "9548503",
+				name: "Jennifer Lawrence",
+				img: "https://picsum.photos/200"
+			},
 			isAdmin: false,
 			isBusy: true
 		}
