73e3de889b5ef1f25855b3c317908475013e586f
Dodiz <dodiz69@gmail.com>
2021-11-06 12:56:44 +0100

    fixed modal

--- src/components/common/pg-popup.jsx
+++ src/components/common/pg-popup.jsx
@@ -7,20 +7,24 @@ import QuickMessage from "./quick-message"
 const PgPopup = ({ data, onClose }) => {
 	return (
 		<Modal small centered show onHide={onClose}>
-			<div className="flex">
+			<div className="flex start">
 				<img className="pg-p__avatar" alt="" src={data.pg.img} />
 				<Text as="h3" className="no-margin">
 					{data.pg.name}
 				</Text>
 			</div>
 			<div className="pg-p">
-				<Text as="h6" className="no-margin pg-p__title">
+				<Text className="no-margin pg-p__title">
 					<em>Utente: </em>
 					{data.name}
 				</Text>
-				<Text as="h6" className="no-margin pg-p__title">
+				<Text className="no-margin pg-p__title">
 					<em>Status: </em>
 					{data.isBusy ? "Occupato" : "Disponibile"}
+				</Text>
+				<Text className="no-margin pg-p__title">
+					<em>Ruolo: </em>
+					Admin
 				</Text>
 				<FrameCorners className="pg-p__button">
 					<Text>Accedi alla scheda</Text>
