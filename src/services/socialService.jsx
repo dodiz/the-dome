d8fd3e1a061722420ad22d99679bccdf2a63186c
Dodiz <dodiz69@gmail.com>
2021-11-06 20:57:54 +0100

    added pg for social ui

--- src/services/socialService.jsx
+++ src/services/socialService.jsx
@@ -2,35 +2,75 @@ function getPosts() {
 	const posts = [
 		{
 			_id: Math.random(),
-			user: "Michael Douglas",
-			avatar: "https://picsum.photos/200",
+			name: "test",
+			pg: {
+				_id: "9384uijre",
+				name: "Michael Douglas",
+				img: "https://picsum.photos/200"
+			},
 			content:
 				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
 			date: "qualche secondo fa"
 		},
 		{
 			_id: Math.random(),
-			user: "Julia Roberts",
-			avatar: "https://picsum.photos/200",
+			name: "toipwtpori",
+			pg: {
+				_id: "ifjkdljfksd",
+				name: "Julia Roberts",
+				img: "https://picsum.photos/200"
+			},
 			content:
 				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
 			date: "2 giorni fa"
 		},
 		{
 			_id: Math.random(),
-			user: "Jack Nicholson",
-			avatar: "https://picsum.photos/200",
-			content:
-				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
-			date: "1239219"
-		},
-		{
-			_id: Math.random(),
-			user: "Will Smith",
-			avatar: "https://picsum.photos/200",
+			name: "test",
+			pg: {
+				_id: "9384uijre",
+				name: "Michael Douglas",
+				img: "https://picsum.photos/200"
+			},
 			content:
 				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
 			date: "qualche secondo fa"
+		},
+		{
+			_id: Math.random(),
+			name: "toipwtpori",
+			pg: {
+				_id: "ifjkdljfksd",
+				name: "Julia Roberts",
+				img: "https://picsum.photos/200"
+			},
+			content:
+				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
+			date: "2 giorni fa"
+		},
+		{
+			_id: Math.random(),
+			name: "test",
+			pg: {
+				_id: "9384uijre",
+				name: "Michael Douglas",
+				img: "https://picsum.photos/200"
+			},
+			content:
+				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
+			date: "qualche secondo fa"
+		},
+		{
+			_id: Math.random(),
+			name: "toipwtpori",
+			pg: {
+				_id: "ifjkdljfksd",
+				name: "Julia Roberts",
+				img: "https://picsum.photos/200"
+			},
+			content:
+				"Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laudantium iure voluptatum sint. Quisquam, temporibus labore maxime quam quas facere repudiandae. Fuga error fugiat nulla.",
+			date: "2 giorni fa"
 		}
 	]
 	return posts
