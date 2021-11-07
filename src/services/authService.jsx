704c0437bbe9c3dec4a46aee7db3a0c641187520
Dodiz <dodiz69@gmail.com>
2021-11-07 12:04:38 +0100

    signup function

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -1,3 +1,19 @@
+import { toast } from "react-toastify"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+
+async function signUp(username, email, password) {
+	const auth = getAuth()
+	createUserWithEmailAndPassword(auth, email, password)
+		.then(userCredential => {
+			toast.success("Welcome to the dome")
+		})
+		.catch(error => {
+			const errorCode = error.code
+			const errorMessage = error.message
+			// ..
+		})
+}
+
 async function login(email, password) {
 	return {
 		displayName: email
