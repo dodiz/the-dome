e961b21e9b00a07d5e045070a97ec916b580129c
Dodiz <dodiz69@gmail.com>
2021-11-08 17:52:03 +0100

    added verify component

--- src/components/verify.jsx
+++ src/components/verify.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react"
 import { Text, Button, FrameCorners } from "@arwes/core"
 
-import { useAuth } from "../context/authContext"
+import authService from "../services/authService"
+import { toast } from "react-toastify"
 
-const Verify = ({ history }) => {
+const Verify = ({ history, location }) => {
 	const [isVerified, setIsVerified] = useState(false)
-	const user = useAuth()
 
 	useEffect(() => {
-		setTimeout(() => {
-			setIsVerified(true)
-		}, [1000])
+		const params = new URLSearchParams(location.search)
+		const code = params.get("oobCode")
+		authService
+			.verifyEmail(code)
+			.then(() => {
+				setIsVerified(true)
+			})
+			.catch(e => toast.error(e))
 	}, [])
 
 	return (
@@ -20,8 +25,9 @@ const Verify = ({ history }) => {
 				{isVerified && (
 					<Text>Il tuo account è stato verificato con successo</Text>
 				)}
-				<div className="form-btn btn mt-1">
+				<div className="form-btn btn mt-1 m-auto">
 					<Button
+						className="m-auto"
 						FrameComponent={FrameCorners}
 						onClick={() => history.push("/login")}>
 						Accedi al login
