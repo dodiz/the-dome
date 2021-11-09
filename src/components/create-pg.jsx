6711d183e715592ea8c35b9f27083e97a7833610
Dodiz <dodiz69@gmail.com>
2021-11-09 16:21:41 +0100

    some style for create-pg

--- src/components/create-pg.jsx
+++ src/components/create-pg.jsx
@@ -11,10 +11,11 @@ class CreatePg extends Form {
 	render() {
 		return (
 			<div className="land__content">
+				<div className="line extend" />
 				<Text as="h1" className="m-auto">
 					Creazione del personaggio
 				</Text>
-				<Text as="div">
+				<Text as="div" className="mb-1">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
 					explicabo praesentium sapiente laboriosam nemo fuga porro amet natus
 					totam recusandae temporibus fugit velit, est ipsa ratione ab vitae
@@ -22,6 +23,7 @@ class CreatePg extends Form {
 					Illum repudiandae dolore consequuntur voluptas aspernatur. Atque
 					sapiente corrupti distinctio provident tenetur eum aliquam?
 				</Text>
+				<div className="line extend" />
 			</div>
 		)
 	}
