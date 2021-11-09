de9819a4f65943ded19a954d4b5c34d6596ae1c8
Dodiz <dodiz69@gmail.com>
2021-11-09 16:35:10 +0100

    moved select

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from "react"
-import Select from "./select"
 import { Button, FrameCorners, Text } from "@arwes/core"
+
+const Select = ({ name, label, error, options, ...rest }) => {
+	return (
+		<div className="form-group">
+			<label htmlFor={name}>{label}</label>
+			<select name={name} className="form-control" {...rest}>
+				{options.map(option => (
+					<option key={option._id} value={option._id}>
+						{option.label}
+					</option>
+				))}
+			</select>
+			{error && <div className="alert alert-danger m-2">{error}</div>}
+		</div>
+	)
+}
 
 const Input = ({ name, label, error, ...rest }) => {
 	return (
