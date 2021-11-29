b3db060c2e7ff0f9bacab349f6536269daa8f982
Dodiz <dodiz69@gmail.com>
2021-11-29 17:57:26 +0100

    styled indicator

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,109 +1,105 @@
 import React, { Component } from "react"
 import anime from "animejs"
-import { FrameCorners } from "@arwes/core"
+import { FrameCorners, FrameBox, Text, List } from "@arwes/core"
+
+import withSound from "./../hoc/withSound"
+
+import { locations } from "../config/locations"
 
 import "../css/map.css"
 
-const locations = [
-	{
-		_id: "deadly_beach",
-		label: "Deadly Beach",
-		x: 440,
-		y: 250
-	},
-	{
-		_id: "gulf_of_whales",
-		label: "Gulf of whales",
-		x: 870,
-		y: 100
-	},
-	{
-		_id: "muddy_lake",
-		label: "Muddy lake",
-		x: 990,
-		y: 300
-	},
-	{
-		_id: "tide_sand",
-		label: "Tide sand",
-		x: 1200,
-		y: 330
-	},
-	{
-		_id: "barren_canyon",
-		label: "Barren canyon",
-		x: 700,
-		y: 400
-	},
-	{
-		_id: "dirty_waters",
-		label: "Dirty waters",
-		x: 1050,
-		y: 410
-	},
-	{
-		_id: "rocky_plain",
-		label: "Rocky plain",
-		x: 1500,
-		y: 450
-	},
-	{
-		_id: "fireflies_forest",
-		label: "Fireflies forest",
-		x: 850,
-		y: 550
-	},
-	{
-		_id: "the_hole",
-		label: "The Hole",
-		x: 1170,
-		y: 580
-	},
-	{
-		_id: "shady_valley",
-		label: "Shady valley",
-		x: 870,
-		y: 700
-	},
-	{
-		_id: "sharp_mountains",
-		label: "Sharp Mountains",
-		x: 1100,
-		y: 970
-	}
-]
-
-const rect = 100
+const rect = 50
 const smallRect = 20
 const viewPort = {
 	x: 1980,
 	y: 1080
 }
 
+const SvgMap = ({ x, y, onSelectLocation }) => (
+	<svg
+		preserveAspectRatio="none"
+		className="map__indicator"
+		viewBox={`0 0 ${viewPort.x} ${viewPort.y}`}>
+		<linearGradient
+			spreadMethod="pad"
+			id="text-grad"
+			x1="0%"
+			y1="100%"
+			x2="0%"
+			y2="0%">
+			<stop offset="0%" style={{ stopColor: "#666", stopOpacity: 1 }} />
+			<stop offset="80%" style={{ stopColor: "#fff", stopOpacity: 1 }} />
+		</linearGradient>
+
+		<g className="map__indicator-g">
+			{/* VERTICAL LINE */}
+			<path d={"M " + x + " 0 " + "V 1080"} className="map__indicator-line" />
+			{/* HORIZONTAL LINE */}
+			<path d={"M 0 " + y + " H 1980"} className="map__indicator-line" />
+			{/* INDICATOR */}
+			<circle
+				cx={x}
+				cy={y}
+				r={rect}
+				width={rect}
+				height={rect}
+				strokeDasharray={`${rect} ${rect * 4 - rect}`}
+				strokeDashoffset={rect * 4}
+				className="map__indicator-square "
+			/>
+			<circle
+				cx={x}
+				cy={y}
+				r={smallRect}
+				className="map__indicator-circle map__indicator-line"
+			/>
+		</g>
+		{/* TEXT LOCATIONS */}
+		{locations.map(location => (
+			<text
+				className="map__indicator-text"
+				dominant-baseline="middle"
+				fontSize={23}
+				onClick={() => onSelectLocation(location)}
+				x={location.x}
+				y={location.y}
+				text-anchor="middle">
+				{location.label}
+			</text>
+		))}
+	</svg>
+)
+
+const BoxMap = ({ location, chats }) => (
+	<FrameBox
+		style={{
+			position: "absolute",
+			top: "15px",
+			right: "15px",
+			width: "300px",
+			height: "200px",
+			zIndex: 2
+		}}>
+		<Text as="h3">{location}</Text>
+		<ul>
+			{chats.map(chat => (
+				<li key={chat._id}>{chat.label}</li>
+			))}
+		</ul>
+	</FrameBox>
+)
+
 class Map extends Component {
 	state = {
 		x: 100,
-		y: 100
+		y: 100,
+		location: "",
+		chats: []
 	}
-
-	mapRef = React.createRef()
 
 	componentDidMount() {
 		this.draw()
-		this.mapRef.current.addEventListener("click", this.handleTracking)
 	}
-
-	componentWillUnmount() {
-		this.mapRef.current.removeEventListener("click", this.handleTracking)
-	}
-
-	handleTracking = e => {
-		var rect = e.target.getBoundingClientRect()
-		var x = ((e.clientX - rect.left) * 1980) / rect.width
-		var y = ((e.clientY - rect.top) * 1080) / rect.height
-		this.handleIndicator(x, y)
-	}
-
 	draw = () => {
 		anime({
 			targets: ".map__pattern",
@@ -122,96 +118,30 @@ class Map extends Component {
 		})
 	}
 
-	handleIndicator = (x, y) => {
-		this.setState({ x, y })
+	handleZoneSelection = location => {
+		const { x, y, label, chats } = location
+		this.props.sounds.assemble.play()
+		this.setState({ x, y, chats, location: label })
 	}
-
 	render() {
-		const { x, y } = this.state
+		const { x, y, location, chats } = this.state
 		return (
-			<FrameCorners
-				origins={["left", "top", "right", "bottom"]}
-				palette="secondary"
-				cornerWidth={2}
-				animator={{ animate: this.state.animate }}
-				cornerLength={40}
-				showContentLines
-				hideShapes>
-				<div ref={this.mapRef} className="map">
-					<div className="map__pattern" />
-					<svg
-						preserveAspectRatio="none"
-						className="map__indicator"
-						viewBox={`0 0 ${viewPort.x} ${viewPort.y}`}>
-						<linearGradient
-							spreadMethod="pad"
-							id="text-grad"
-							x1="0%"
-							y1="100%"
-							x2="0%"
-							y2="0%">
-							<stop offset="0%" style={{ stopColor: "#666", stopOpacity: 1 }} />
-							<stop
-								offset="80%"
-								style={{ stopColor: "#fff", stopOpacity: 1 }}
-							/>
-						</linearGradient>
-
-						<g className="map__indicator-group">
-							{/* VERTICAL LINE */}
-							<path
-								d={"M " + x + " 0 " + "V 1080"}
-								className="map__indicator-line"
-							/>
-							{/* HORIZONTAL LINE */}
-							<path
-								d={"M 0 " + y + " H 1980"}
-								className="map__indicator-line"
-							/>
-							{/* INDICATOR */}
-							<rect
-								x={x - rect / 2}
-								y={y - rect / 2}
-								width={rect}
-								height={rect}
-								className="map__indicator-square small"
-							/>
-							<rect
-								style={{
-									strokeDasharray: rect / 2,
-									strokeDashoffset: rect / 4
-								}}
-								x={x - rect / 2}
-								y={y - rect / 2}
-								width={rect}
-								height={rect}
-								className="map__indicator-square"
-							/>
-							<circle
-								cx={x}
-								cy={y}
-								r={smallRect}
-								className="map__indicator-square"
-							/>
-						</g>
-						{/* TEXT LOCATIONS */}
-						{locations.map(location => (
-							<text
-								className="map__indicator-text"
-								dominant-baseline="middle"
-								fontSize={23}
-								onClick={() => this.handleIndicator(location.x, location.y)}
-								x={location.x}
-								y={location.y}
-								text-anchor="middle">
-								{location.label}
-							</text>
-						))}
-					</svg>
-				</div>
-			</FrameCorners>
+			<>
+				{location && <BoxMap location={location} chats={chats} />}
+				<FrameCorners
+					palette="secondary"
+					cornerWidth={2}
+					cornerLength={40}
+					showContentLines
+					hideShapes>
+					<div className="map">
+						<div className="map__pattern" />
+						<SvgMap x={x} y={y} onSelectLocation={this.handleZoneSelection} />
+					</div>
+				</FrameCorners>
+			</>
 		)
 	}
 }
 
-export default Map
+export default withSound(Map)
---
+++ src/config/locations.js
@@ -0,0 +1,259 @@
+
+export const locations = [
+    {
+        _id: "deadly_beach",
+        label: "Deadly Beach",
+        x: 440,
+        y: 250,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "gulf_of_whales",
+        label: "Gulf of whales",
+        x: 870,
+        y: 100,
+        chats: [
+            {
+                _id: "location_5",
+                label: "Location 5"
+            },
+            {
+                _id: "location_6",
+                label: "Location 6"
+            },
+            {
+                _id: "location_7",
+                label: "Location 7"
+            }
+        ]
+    },
+    {
+        _id: "muddy_lake",
+        label: "Muddy lake",
+        x: 990,
+        y: 300,
+        chats: [
+            {
+                _id: "location_8",
+                label: "Location 8"
+            },
+            {
+                _id: "location_9",
+                label: "Location 9"
+            },
+            {
+                _id: "location_10",
+                label: "Location 10"
+            }
+        ]
+    },
+    {
+        _id: "tide_sand",
+        label: "Tide sand",
+        x: 1200,
+        y: 330,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "barren_canyon",
+        label: "Barren canyon",
+        x: 700,
+        y: 400,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "dirty_waters",
+        label: "Dirty waters",
+        x: 1050,
+        y: 410,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "rocky_plain",
+        label: "Rocky plain",
+        x: 1500,
+        y: 450,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "fireflies_forest",
+        label: "Fireflies forest",
+        x: 850,
+        y: 550,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "the_hole",
+        label: "The Hole",
+        x: 1170,
+        y: 580,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "shady_valley",
+        label: "Shady valley",
+        x: 870,
+        y: 700,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    },
+    {
+        _id: "sharp_mountains",
+        label: "Sharp Mountains",
+        x: 1100,
+        y: 970,
+        chats: [
+            {
+                _id: "location_1",
+                label: "Location 1"
+            },
+            {
+                _id: "location_2",
+                label: "Location 2"
+            },
+            {
+                _id: "location_3",
+                label: "Location 3"
+            },
+            {
+                _id: "location_4",
+                label: "Location 4"
+            },
+        ]
+    }
+]
