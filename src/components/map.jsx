531f99da28a9860be18a07cd2381931f90897ba2
Dodiz <dodiz69@gmail.com>
2021-11-29 14:25:24 +0100

    styled map indicator

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -6,20 +6,70 @@ import "../css/map.css"
 
 const locations = [
 	{
-		_id: "gulf_of_whales",
-		label: "Gulf of whales",
-		x: 800,
-		y: 150,
-		centerX: 875,
-		centerY: 140
-	},
-	{
 		_id: "deadly_beach",
 		label: "Deadly Beach",
-		x: 350,
-		y: 250,
-		centerX: 410,
-		centerY: 240
+		x: 440,
+		y: 250
+	},
+	{
+		_id: "gulf_of_whales",
+		label: "Gulf of whales",
+		x: 870,
+		y: 100
+	},
+	{
+		_id: "muddy_lake",
+		label: "Muddy lake",
+		x: 990,
+		y: 300
+	},
+	{
+		_id: "tide_sand",
+		label: "Tide sand",
+		x: 1200,
+		y: 330
+	},
+	{
+		_id: "barren_canyon",
+		label: "Barren canyon",
+		x: 700,
+		y: 400
+	},
+	{
+		_id: "dirty_waters",
+		label: "Dirty waters",
+		x: 1050,
+		y: 410
+	},
+	{
+		_id: "rocky_plain",
+		label: "Rocky plain",
+		x: 1500,
+		y: 450
+	},
+	{
+		_id: "fireflies_forest",
+		label: "Fireflies forest",
+		x: 850,
+		y: 550
+	},
+	{
+		_id: "the_hole",
+		label: "The Hole",
+		x: 1170,
+		y: 580
+	},
+	{
+		_id: "shady_valley",
+		label: "Shady valley",
+		x: 870,
+		y: 700
+	},
+	{
+		_id: "sharp_mountains",
+		label: "Sharp Mountains",
+		x: 1100,
+		y: 970
 	}
 ]
 
@@ -32,8 +82,8 @@ const viewPort = {
 
 class Map extends Component {
 	state = {
-		x: viewPort.x / 2,
-		y: viewPort.y / 2
+		x: 100,
+		y: 100
 	}
 
 	mapRef = React.createRef()
@@ -51,16 +101,10 @@ class Map extends Component {
 		var rect = e.target.getBoundingClientRect()
 		var x = ((e.clientX - rect.left) * 1980) / rect.width
 		var y = ((e.clientY - rect.top) * 1080) / rect.height
-		this.setState({ x, y })
+		this.handleIndicator(x, y)
 	}
 
 	draw = () => {
-		anime({
-			targets: ".map__indicator-square, .map__indicator-line",
-			stroke: [0, 7],
-			duration: 1000,
-			delay: 500
-		})
 		anime({
 			targets: ".map__pattern",
 			scale: {
@@ -99,48 +143,70 @@ class Map extends Component {
 						preserveAspectRatio="none"
 						className="map__indicator"
 						viewBox={`0 0 ${viewPort.x} ${viewPort.y}`}>
-						<linearGradient id="text-grad" x1="0%" x2="0%" y1="100%" y2="100%">
-							<stop offset="0%" style={{ stopColor: "white" }} />
-							<stop offset="100%" style={{ stopColor: "white" }} />
+						<linearGradient
+							spreadMethod="pad"
+							id="text-grad"
+							x1="0%"
+							y1="100%"
+							x2="0%"
+							y2="0%">
+							<stop offset="0%" style={{ stopColor: "#666", stopOpacity: 1 }} />
+							<stop
+								offset="80%"
+								style={{ stopColor: "#fff", stopOpacity: 1 }}
+							/>
 						</linearGradient>
-						<path
-							d={"M " + x + " 0 " + "V 1080"}
-							className="map__indicator-line"
-						/>
-						<path d={"M 0 " + y + " H 1980"} className="map__indicator-line" />
+
+						<g className="map__indicator-group">
+							{/* VERTICAL LINE */}
+							<path
+								d={"M " + x + " 0 " + "V 1080"}
+								className="map__indicator-line"
+							/>
+							{/* HORIZONTAL LINE */}
+							<path
+								d={"M 0 " + y + " H 1980"}
+								className="map__indicator-line"
+							/>
+							{/* INDICATOR */}
+							<rect
+								x={x - rect / 2}
+								y={y - rect / 2}
+								width={rect}
+								height={rect}
+								className="map__indicator-square small"
+							/>
+							<rect
+								style={{
+									strokeDasharray: rect / 2,
+									strokeDashoffset: rect / 4
+								}}
+								x={x - rect / 2}
+								y={y - rect / 2}
+								width={rect}
+								height={rect}
+								className="map__indicator-square"
+							/>
+							<circle
+								cx={x}
+								cy={y}
+								r={smallRect}
+								className="map__indicator-square"
+							/>
+						</g>
+						{/* TEXT LOCATIONS */}
 						{locations.map(location => (
 							<text
-								x={location.x}
-								onClick={() =>
-									this.handleIndicator(location.centerX, location.centerY)
-								}
-								y={location.y}
 								className="map__indicator-text"
-								fill="url(#text-grad)">
+								dominant-baseline="middle"
+								fontSize={23}
+								onClick={() => this.handleIndicator(location.x, location.y)}
+								x={location.x}
+								y={location.y}
+								text-anchor="middle">
 								{location.label}
 							</text>
 						))}
-						<rect
-							x={x - rect / 2}
-							y={y - rect / 2}
-							width={rect}
-							height={rect}
-							className="map__indicator-square small"
-						/>
-						<rect
-							style={{ strokeDasharray: rect / 2, strokeDashoffset: rect / 4 }}
-							x={x - rect / 2}
-							y={y - rect / 2}
-							width={rect}
-							height={rect}
-							className="map__indicator-square"
-						/>
-						<circle
-							cx={x}
-							cy={y}
-							r={smallRect}
-							className="map__indicator-square"
-						/>
 					</svg>
 				</div>
 			</FrameCorners>
